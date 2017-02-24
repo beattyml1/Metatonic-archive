23df0ec83dcb9a1530feb3277b1a6cae8cfab2b0
Matthew Beatty <beattyml1@gmail.com>
2017-02-24 14:43:56 -0500

    CustomControlsService.ts

--- src/services/CustomControlsService.ts
+++ src/services/CustomControlsService.ts
@@ -3,9 +3,15 @@ export class CustomControlsService {
 
   }
 
-  getCustomControlForType = (type: string) => "";
+  customControls: {};
 
-  addCustom = (type: string, control: any) => {};
+  getCustomControlForType = (type: string) => {
+    return this.customControls[type];
+  }
+
+  addCustom = (type: string, control: any) => {
+    this.customControls[type] = control;
+  };
 }
 
 export const CustomControls = new CustomControlsService();
