69241801a3c9108b37b67315f13a6b602e324481
Matthew Beatty <beattyml1@gmail.com>
2017-04-10 10:29:45 +0100

    Get a static html/js test bed server spun up

---
+++ src/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var app = express();
+
+
+app.use(serveStatic(__dirname.slice(0, -4) + '/built/bin', {'index': ['default.html', 'default.htm']}));
+app.use(serveStatic(__dirname.slice(0, -4) + '/public', {'index': ['default.html', 'default.htm']}));
+app.listen(8080);
+// 
+//connect().use(serveStatic(__dirname.slice(0, -4) + '/built/bin'), { index: true }).listen(8080, function(){
+//    console.log('Server running on 8080 in directory ' + __dirname.slice(0, -4) + '/built/bin');
+//});
