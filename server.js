84146c1142886ad752c97bd855ac149c5067cba3
Matthew Beatty <beattyml1@gmail.com>
2017-04-11 10:09:58 +0100

    Moved server.js out of source since it's not part of source code but more of a test piece

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var serveStatic = require('serve-static');
 var app = express();
 
 
-app.use(serveStatic(__dirname.slice(0, -4) + '/built/bin', {'index': ['default.html', 'default.htm']}));
-app.use(serveStatic(__dirname.slice(0, -4) + '/public', {'index': ['index.html', 'default.htm']}));
+app.use(serveStatic(__dirname + '/built/bin', {'index': ['default.html', 'default.htm']}));
+app.use(serveStatic(__dirname + '/public', {'index': ['index.html', 'default.htm']}));
 app.listen(8080);
 // 
 //connect().use(serveStatic(__dirname.slice(0, -4) + '/built/bin'), { index: true }).listen(8080, function(){
