96f609a5a2d61bb3945a33c220a1e5c8d6f3a9f3
Matthew Beatty <beattyml1@gmail.com>
2017-04-11 10:05:38 +0100

    Added index route

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ var app = express();
 
 
 app.use(serveStatic(__dirname.slice(0, -4) + '/built/bin', {'index': ['default.html', 'default.htm']}));
-app.use(serveStatic(__dirname.slice(0, -4) + '/public', {'index': ['default.html', 'default.htm']}));
+app.use(serveStatic(__dirname.slice(0, -4) + '/public', {'index': ['index.html', 'default.htm']}));
 app.listen(8080);
 // 
 //connect().use(serveStatic(__dirname.slice(0, -4) + '/built/bin'), { index: true }).listen(8080, function(){
